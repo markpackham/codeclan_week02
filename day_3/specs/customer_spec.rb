fced747444aa79c6171b5faa10ea6aac231b4711
mark <markpackham1@gmail.com>
2020-04-08 10:46:18 +0100

    Do some 0 tests

--- day_3/specs/customer_spec.rb
+++ day_3/specs/customer_spec.rb
@@ -6,10 +6,19 @@ require_relative("../customer")
 
 class CustomerTest < MiniTest::Test
   def setup()
-    @customer = Customer.new("Bob")
+    @customer = Customer.new("Bob",50)
+    @customer2 = Customer.new("Hobo",0)
   end
 
   def test_get_name
     assert_equal("Bob", @customer.name)
+  end
+
+  def test_get_wallet__has_money
+    assert_equal(50, @customer.wallet)
+  end
+
+  def test_get_wallet__has_no_money
+    assert_equal(0, @customer2.wallet)
   end
 end
--- day_3/specs/drink_spec.rb
+++ day_3/specs/drink_spec.rb
@@ -7,14 +7,19 @@ require_relative("../drink")
 class DrinkTest < MiniTest::Test
   def setup()
     @drink = Drink.new("Vodka",10)
+    @drink2 = Drink.new("Free Grog",0)
   end
 
   def test_get_name
     assert_equal("Vodka", @drink.name)
   end
 
-  def test_get_price
+  def test_get_price__costs_money
     assert_equal(10, @drink.price)
+  end
+
+  def test_get_price__is_free
+    assert_equal(0, @drink2.price)
   end
 
 end
